353ba80e34453b8d7718f23e14bd3407d3b018e3
Nathan Pabst <nathan.pabst@gmail.com>
2018-05-16 18:40:47 -0500

    promises complete

--- javascripts/locations.js
+++ javascripts/locations.js
@@ -1,7 +1,13 @@
-// const loadLocations = (loadFunction, errorFunction) => {
-//   $.get('./db/locations.json')
-//     .done(loadFunction)
-//     .fail(errorFunction);
-// };
+const loadLocations = () => {
+  return new Promise((resolve, reject) => {
+    $.get('./db/locations.json')
+      .done((data) => {
+        resolve(data);
+      })
+      .fail((error) => {
+        reject('error, dang!', error);
+      });
+  });
+};
 
-// module.exports = loadLocations;
+module.exports = loadLocations;
