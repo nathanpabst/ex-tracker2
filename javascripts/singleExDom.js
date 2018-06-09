327598dd1874563558223716bf35c17320ea6619
Nathan Pabst <nathan.pabst@gmail.com>
2018-06-08 20:51:29 -0500

    fixed the single ex card but completely eff'd the location cards

--- javascripts/singleExDom.js
+++ javascripts/singleExDom.js
@@ -2,17 +2,15 @@ const writeSingleEx = (deet) => {
   let output = '';
   output += `<div class="container-fluid">
                 <div class="row ex">
-                  <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                      <img class="ex-pic" src="${deet.imageURL}" alt="profile-pic">
-                      <div class="caption ex-deets">
-                        <h3>NAME: ${deet.name}</h3>
-                        <h3>AGE: ${deet.age}</h3>
-                        <h3>FLAWS: </h3>
-                        <p>${deet.flaws}</p>
-                        <p><button id="${deet.id}" class="btn btn-primary backButton">Back</button>
-                      </div>
-                    </div>
+                  <div class="col-md-4 col-sm-offset-2 text-center">
+                    <img class="ex-pic" src="${deet.imageURL}" alt="profile-pic">
+                  </div>
+                  <div class="col-sm-4 ex-deets">
+                    <h3>NAME: ${deet.name}</h3>
+                    <h3>AGE: ${deet.age}</h3>
+                    <h3>FLAWS: </h3>
+                    <p>${deet.flaws}</p>
+                    <p><button id="${deet.id}" class="btn btn-primary backButton">Back</button>
                   </div>
                 </div>
               </div>`;
